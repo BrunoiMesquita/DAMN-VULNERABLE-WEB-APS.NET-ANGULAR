471ed8a84cd80b85d1b24a05aa6cb8ec0cce6aee
dahln <dahln@outlook.com>
2018-03-13 10:05:58 -0600

    ssl working for local docker-compose

--- tagScript.sh
+++ tagScript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker-compose bundle
-docker tag cdp_db dahln/private:cdp_db.18.3.12.4
-docker tag cdp_api dahln/private:cdp_api.18.3.12.4
-docker tag cdp_ng dahln/private:cdp_nginx.18.3.12.4
+docker tag cdp_db dahln/private:cdp_db.18.3.13.1
+docker tag cdp_api dahln/private:cdp_api.18.3.13.1
+docker tag cdp_ng dahln/private:cdp_nginx.18.3.13.1
 
-docker push dahln/private:cdp_db.18.3.12.4
-docker push dahln/private:cdp_api.18.3.12.4
-docker push dahln/private:cdp_nginx.18.3.12.4
+docker push dahln/private:cdp_db.18.3.13.1
+docker push dahln/private:cdp_api.18.3.13.1
+docker push dahln/private:cdp_nginx.18.3.13.1
 
