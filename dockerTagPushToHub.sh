55f7c58b77460dad68e394fb27bba3885e290f9d
dahln <dahln@outlook.com>
2018-03-13 16:46:59 -0600

    trying to change the database connection to mongodb atlas

--- dockerTagPushToHub.sh
+++ dockerTagPushToHub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker tag cdp_db:latest dahln/private:cdp_db.18.3.13.5
-docker tag cdp_api:latest dahln/private:cdp_api.18.3.13.5
-docker tag cdp_nginx:latest dahln/private:cdp_nginx.18.3.13.5
+docker tag cdp_db:latest dahln/private:cdp_db.18.3.13.8
+docker tag cdp_api:latest dahln/private:cdp_api.18.3.13.8
+docker tag cdp_nginx:latest dahln/private:cdp_nginx.18.3.13.8
 
-docker push dahln/private:cdp_db.18.3.13.5
-docker push dahln/private:cdp_api.18.3.13.5
-docker push dahln/private:cdp_nginx.18.3.13.5
+docker push dahln/private:cdp_db.18.3.13.8
+docker push dahln/private:cdp_api.18.3.13.8
+docker push dahln/private:cdp_nginx.18.3.13.8
 
