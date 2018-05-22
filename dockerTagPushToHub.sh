5d50af50cde51b22657f04215053b65ed0775b68
Dahln Farnes <dahln@outlook.com>
2018-05-22 13:12:19 -0600

    Project rename and new content.

--- dockerTagPushToHub.sh
+++ dockerTagPushToHub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker tag cdp_api:latest dahln/private:cdp_api.18.5.21.1
-docker tag cdp_nginx:latest dahln/private:cdp_nginx.18.5.21.1
+docker tag glubfish_api:latest dahln/glubfish:glubfish_api.18.5.21.1
+docker tag glubfish_nginx:latest dahln/glubfish:glubfish_nginx.18.5.21.1
 
-docker push dahln/private:cdp_api.18.5.21.1
-docker push dahln/private:cdp_nginx.18.5.21.1
+docker push dahln/glubfish:glubfish_api.18.5.21.1
+docker push dahln/glubfish:glubfish_nginx.18.5.21.1
 
