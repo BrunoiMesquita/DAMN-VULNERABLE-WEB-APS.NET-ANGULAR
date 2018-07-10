f4a6aa01dd70f51b182abed7fad19a15b1e8b61a
Dahln Farnes <dahln@outlook.com>
2018-07-09 22:21:18 -0600

    Pushed new image with cert fix for demo. Removed .sh file, left the bat.

--- dockerTagPushToHub.bat
+++ dockerTagPushToHub.bat
@@ -1,6 +1,6 @@
-docker tag glubfish_api:latest dahln/glubfish:glubfish_api.18.7.8.1
-docker tag glubfish_nginx:latest dahln/glubfish:glubfish_nginx.18.7.8.1
+docker tag glubfish_api:latest dahln/glubfish:glubfish_api.18.7.9.1
+docker tag glubfish_nginx:latest dahln/glubfish:glubfish_nginx.18.7.9.1
 
-docker push dahln/glubfish:glubfish_api.18.7.8.1
-docker push dahln/glubfish:glubfish_nginx.18.7.8.1
+docker push dahln/glubfish:glubfish_api.18.7.9.1
+docker push dahln/glubfish:glubfish_nginx.18.7.9.1
 
--- dockerTagPushToHub.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker tag glubfish_api:latest dahln/glubfish:glubfish_api.18.7.8.1
-docker tag glubfish_nginx:latest dahln/glubfish:glubfish_nginx.18.7.8.1
-
-docker push dahln/glubfish:glubfish_api.18.7.8.1
-docker push dahln/glubfish:glubfish_nginx.18.7.8.1
-
