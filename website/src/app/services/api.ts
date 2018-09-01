df0d5daac124bb1f31d16b6925b08f3d4d63f420
Dahln Farnes <dahln@outlook.com>
2018-09-01 09:51:48 -0600

    UI rebranding from glubfish to damnstack

--- website/src/app/services/api.ts
+++ website/src/app/services/api.ts
@@ -16,7 +16,6 @@ export class Api {
       }
     }
     options = new RequestOptions({withCredentials: true});
-    // apiRoot = document.domain === 'localhost' ? '//localhost/api' : '//ngnetmongo.glubfish.com/api'
 
     public registerUser(email: string, password: string, confirmPassword: string) {
         let user = { email, password, confirmPassword };
