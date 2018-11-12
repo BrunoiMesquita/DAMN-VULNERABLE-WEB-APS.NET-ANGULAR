2819b7986b85e4b0950cddff300984e4eb8c6aa4
Dahln Farnes <dahln@outlook.com>
2018-11-12 11:07:29 -0700

    4.1.0: UI Updates

--- dockerTagPushToHub.bat
+++ dockerTagPushToHub.bat
@@ -1,5 +1,5 @@
-docker tag stack_app:latest damnstack/stack:stack_app.4.0.3
-docker tag stack_nginx:latest damnstack/stack:stack_nginx.4.0.3
-docker push damnstack/stack:stack_app.4.0.3
-docker push damnstack/stack:stack_nginx.4.0.3
+docker tag stack_app:latest damnstack/stack:stack_app.4.1.0
+docker tag stack_nginx:latest damnstack/stack:stack_nginx.4.1.0
+docker push damnstack/stack:stack_app.4.1.0
+docker push damnstack/stack:stack_nginx.4.1.0
 
