433df8f45d38c821e4f32db62ac2bfecb58e209e
Dahln Farnes <dahln@outlook.com>
2018-12-12 08:18:34 -0700

    Fixed nginx config file to use the new domain.

--- dockerTagPushToHub.bat
+++ dockerTagPushToHub.bat
@@ -1,5 +1,5 @@
-docker tag stack_app:latest damnstack/stack:stack_app.4.2.0
-docker tag stack_nginx:latest damnstack/stack:stack_nginx.4.2.0
-docker push damnstack/stack:stack_app.4.2.0
-docker push damnstack/stack:stack_nginx.4.2.0
+docker tag stack_app:latest damnstack/stack:stack_app.4.2.1
+docker tag stack_nginx:latest damnstack/stack:stack_nginx.4.2.1
+docker push damnstack/stack:stack_app.4.2.1
+docker push damnstack/stack:stack_nginx.4.2.1
 
