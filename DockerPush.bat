be92a633b860c5be7a84f6dcd9883d0c1aad5198
Dahln Farnes <dahln@outlook.com>
2019-01-02 21:45:49 -0700

    Fixes to nginx configuration

--- DockerPush.bat
+++ DockerPush.bat
@@ -1,5 +1,5 @@
-docker tag damn_api:latest dahln/damn-deploy:damn_api.5.0.0
-docker tag damn_proxy:latest dahln/damn-deploy:damn_proxy.5.0.0
-docker push dahln/damn-deploy:damn_api.5.0.0
-docker push dahln/damn-deploy:damn_proxy.5.0.0
+docker tag damn_api:latest dahln/damn-deploy:damn_api.5.0.4
+docker tag damn_proxy:latest dahln/damn-deploy:damn_proxy.5.0.4
+docker push dahln/damn-deploy:damn_api.5.0.4
+docker push dahln/damn-deploy:damn_proxy.5.0.4
 
